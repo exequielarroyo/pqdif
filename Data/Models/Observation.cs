a6aab37c58f5cb99fa4f426bab14d083797f7cca
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-05-14 08:03:38 +0800

    Initial PQDIF Models

--- Data/Models/Observation.cs
+++ Data/Models/Observation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gemstone.PQDIF.Logical;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,10 @@ namespace Data.Models
     public class Observation
     {
         public string Name { get; set; }
-
+        public DbSet<ChannelInstance> ChannelInstances { get; set; }
+        public TriggerMethod TriggerMethod { get; set; }
+        public DateTime TimeTriggered { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime CreateTime { get; set; }
     }
 }
---
+++ Data/Models/SeriesInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class SeriesInstance
+    {
+        public int Offset { get; set; }
+        public int Scale { get; set; }
+        public string Values { get; set; }
+    }
+}
